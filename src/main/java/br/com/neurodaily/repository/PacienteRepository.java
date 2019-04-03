82cca60e9c1f88c99fc04e99ab65043c18daa478
Leonardo Machado <leonardomachadosi@hotmail.com>
2019-04-03 14:06:21 -0300

    subindo

--- src/main/java/br/com/neurodaily/repository/PacienteRepository.java
+++ src/main/java/br/com/neurodaily/repository/PacienteRepository.java
@@ -3,6 +3,7 @@ package br.com.neurodaily.repository;
 import br.com.neurodaily.model.domain.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,11 +11,11 @@ public interface PacienteRepository extends JpaRepository<Paciente, Long> {
 
     Paciente findById(long id);
 
-    @Query(value = "select *" +
-            " from ND10_PACIENTE ND10" +
-            " inner join ND01_PESSOA ND01" +
+    @Query(value = "SELECT *" +
+            " FROM ND10_PACIENTE ND10" +
+            " INNER JOIN ND01_PESSOA ND01" +
             " ON ND01.ND01_COD_PESSOA = ND10.FKND10ND01_COD_PESSOA" +
-            " WHERE ND01.ND01_NOME LIKE '%?%' ", nativeQuery = true)
-    List<Paciente> findPacienteByPessoaNome(String nome);
+            " WHERE ND01.ND01_NOME LIKE %:nome% ", nativeQuery = true)
+    List<Paciente> findPacienteByPessoaNome(@Param("nome") String nome);
 
 }
